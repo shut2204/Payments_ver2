7a2026da183fc5c11144401f105e6edf444a53e5
Maks Nikolaienko <maks2002sv@gmail.com>
2022-08-14 09:11:15 +0300

    db

--- src/main/java/com/my/db/entity/Card.java
+++ src/main/java/com/my/db/entity/Card.java
@@ -1,4 +1,41 @@
 package com.my.db.entity;
 
 public class Card {
+
+    private int idcard;
+    private int idcustomer;
+    private int balance;
+    private String name_card;
+
+    public int getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(int idcard) {
+        this.idcard = idcard;
+    }
+
+    public int getIdcustomer() {
+        return idcustomer;
+    }
+
+    public void setIdcustomer(int idcustomer) {
+        this.idcustomer = idcustomer;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getName_card() {
+        return name_card;
+    }
+
+    public void setName_card(String name_card) {
+        this.name_card = name_card;
+    }
 }
--- src/main/java/com/my/db/entity/Customer.java
+++ src/main/java/com/my/db/entity/Customer.java
@@ -1,4 +1,68 @@
 package com.my.db.entity;
 
 public class Customer {
+
+    private int idcustomer;
+    private String phone_number;
+    private String first_name;
+    private String last_name;
+    private String login;
+    private String password_customer;
+    private String role;
+
+    public int getIdcustomer() {
+        return idcustomer;
+    }
+
+    public void setIdcustomer(int idcustomer) {
+        this.idcustomer = idcustomer;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword_customer() {
+        return password_customer;
+    }
+
+    public void setPassword_customer(String password_customer) {
+        this.password_customer = password_customer;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
